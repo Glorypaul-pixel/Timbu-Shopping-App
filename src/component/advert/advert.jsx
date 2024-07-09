cbc5f7effa9b8db75f9747dfbdb14637ec35ae7c
Glorypixel <ifeomaglory1998@gmail.com>
2024-07-09 12:36:00 +0100

    HNG wont kill me sha

--- src/component/advert/advert.jsx
+++ src/component/advert/advert.jsx
@@ -2,7 +2,7 @@ import "./advert.css";
 
 const advert = () => {
   return (
-    <div className="shop-advert cursor-default">
+    <div className="shop-advert cursor-default mt-14">
       <h1>#Shop Now</h1>
       <p>Check out our collection</p>
     </div>
